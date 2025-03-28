ea62c6b8ea87208ec59d18a096639fb277fa1492
axell <mail@axell.me>
2025-03-28 22:42:47 +0100

    added logo

--- rays/rayutil.js
+++ rays/rayutil.js
@@ -7,7 +7,6 @@ const rayutil = {
 }
 
 addEventListener("load", (event) => {
-    console.log('ray: document load')
     if ("$${Message}" != "") {
         console.log('inserting message')
         const html = '<div id="__ray-notification" style="z-index: 50000; position: fixed; height: max-content; width: max-content; padding: 0.5rem; top: 0.5rem; left: 0.5rem; background-color: black; display: flex; flex-direction: row; color: white; border-radius: 0.25rem; align-items: center; transition: opacity 0.5s; opacity: 1;"><svg style="margin-right: 0.25rem;" xmlns="http://www.w3.org/2000/svg" width="20" height="20" $${Icon}></svg><span style="font-family: monospace; font-size: 1rem;">$${Message}</span></div>'
