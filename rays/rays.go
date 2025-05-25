781fcbcb90ce82e600aa6bfd5d01291396b89b9d
axell <mail@axell.me>
2025-05-25 13:48:37 +0200

    fix small issue

--- rays/rays.go
+++ rays/rays.go
@@ -7,10 +7,10 @@ import (
 var Version = "unknown version"
 
 func main() {
+	assignDotSlash()
 	if (!checkPerms()) {
 		rlog.Fatal("To use the ray CLI or to launch rays you need to run as root or using sudo")
 	}
-	assignDotSlash()
 	if (len(os.Args) == 1) {
 		rlog.Fatal("No arguments passed!")
 	}
