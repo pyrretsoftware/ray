9f8e145a9a01518537d55083167cc0c86c0ebd7c
axell <mail@axell.me>
2025-09-28 21:23:04 +0200

    fix potential panic

--- rays/getProcessPort.go
+++ rays/getProcessPort.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"os/exec"
 	"runtime"
 	"strconv"
@@ -22,12 +23,14 @@ var platformPidSeperator = map[string]string{
 	"linux" : "pid=",
 }
 
-//todo: this shouldnt panic 
-func parse(content string) map[string][]string {
-	table := make(map[string][]string)
+func parse(content string) (map[string][]string, error) {
+	table := map[string][]string{}
 
 	for _, line := range strings.Split(strings.ReplaceAll(content, "\r", ""), "\n") {
 		if (!strings.Contains(line, platformListen[runtime.GOOS])) {continue}
+		if len(strings.Split(line, platformPidSeperator[runtime.GOOS])) == 0 {
+			return map[string][]string{}, errors.New("pidSeperator not present, ignoring")
+		}
 
 		_p1 := strings.Split(line, ":")[1]
 		port := strings.ReplaceAll(strings.Split(_p1, " ")[0], " ", "")
@@ -35,23 +38,28 @@ func parse(content string) map[string][]string {
 
 		table[pid] = append(table[pid], port)
 	}
-	return table
+	return table, nil
 }
 
 func getProcessPorts(pid int) []string {
 	val, contains := platformCommand[runtime.GOOS]
-	if (!contains) {
+	if !contains {
 		rlog.Notify("Rays is running on an unsupported platform, and were not able the resolve certain process information.", "warn")
 		return []string{}
 	}
 
 	cmd := exec.Command(val[0], val[1])
 	out, err := cmd.Output()
-	if (err != nil) {
+	if err != nil {
 		rlog.Notify("Rays were not able to resolve certain process information.", "warn")
 		return []string{}
 	}
 
-	table := parse(string(out))
+	table, err := parse(string(out))
+	if err != nil {
+		rlog.Notify("Rays were not able to resolve certain process information.", "warn")
+		return []string{}
+	}
+
 	return table[strconv.Itoa(pid)]
 }
